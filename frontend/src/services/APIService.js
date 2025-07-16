20c2eefd722318f89e7d8dadf613438804df0272
Rodrigo Barros <rodrigopk@gmail.com>
2025-07-16 12:44:00 -0400

    Refactor APIService

--- frontend/src/services/APIService.js
+++ frontend/src/services/APIService.js
@@ -1,79 +1,91 @@
 const BASE_URL = "http://localhost:3000/api/v1";
 
-const APIService = {
-  signIn: async ({ email, password }) => {
-    const response = await fetch(`${BASE_URL}/auth/sign_in`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ email, password })
-    });
+const signIn = async ({ email, password }) => {
+  const response = await fetch(`${BASE_URL}/auth/sign_in`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email, password })
+  });
 
-    if (!response.ok) {
-      const error = await response.json();
-      throw new Error(error?.error || "Login failed");
-    }
-
-    return await response.json();
-  },
-
-  getRewards: async (token) => {
-    const response = await fetch(`${BASE_URL}/rewards`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: "application/json"
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch rewards");
-    }
-
-    return await response.json();
-  },
-  getPoints: async (token) => {
-    const response = await fetch(`${BASE_URL}/points`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        Accept: "application/json"
-      }
-    });
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch points");
-    }
-
-    return await response.json();
-  },
-  getRedemptions: async (token) => {
-    const response = await fetch(`${BASE_URL}/redemptions`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error("Failed to fetch redemptions");
-    }
-    return await response.json();
-  },
-  redeemReward: async (token, rewardId) => {
-    const response = await fetch(`${BASE_URL}/redemptions`, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ reward_id: rewardId }),
-    });
-
-    if (!response.ok) {
-      const errorData = await response.json();
-      throw new Error(errorData.error || "Failed to redeem reward");
-    }
-
-    return await response.json();
+  if (!response.ok) {
+    const error = await response.json();
+    throw new Error(error?.error || "Login failed");
   }
+
+  return await response.json();
+}
+
+const getRewards = async (token) => {
+  const response = await fetch(`${BASE_URL}/rewards`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json"
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch rewards");
+  }
+
+  return await response.json();
+}
+
+const getPoints = async (token) => {
+  const response = await fetch(`${BASE_URL}/points`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json"
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch points");
+  }
+
+  return await response.json();
+}
+
+const getRedemptions = async (token) => {
+  const response = await fetch(`${BASE_URL}/redemptions`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/json"
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch redemptions");
+  }
+
+  return await response.json();
+}
+
+const redeemReward = async (token, rewardId) => {
+  const response = await fetch(`${BASE_URL}/redemptions`, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ reward_id: rewardId }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.error || "Failed to redeem reward");
+  }
+
+  return await response.json();
+}
+
+const APIService = {
+  signIn,
+  getRewards,
+  getPoints,
+  getRedemptions,
+  redeemReward
 };
 
 export default APIService;
